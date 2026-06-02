a9ae14d84d5aa3786a9faa00fa77079a9714780b
Schhneider <girts.sneiders@gmail.com>
2026-06-02 08:56:07 +0300

    laravel remove

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,8 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.tsx'],
-            ssr: 'resources/js/ssr.tsx',
+            input: ['frontend/css/app.css', 'frontend/js/app.tsx'],
+            ssr: 'frontend/js/ssr.tsx',
             refresh: true,
         }),
         react({
